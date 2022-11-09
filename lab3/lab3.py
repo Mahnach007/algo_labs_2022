35bbadd6b7caf987d471173994174e1db551bdac
Vlad Gotovchykov <vladyslav.hotovchykov.ir.2021@lpnu.ua>
2022-11-09 23:06:34 +0100

    Add lab4

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -8,7 +8,7 @@ u = 0
 new_set = set()
 new_list = []
 for i in range(len(pairs) - 1):
-    for j in range(i + 1, len(pairs)): 
+    for j in range(i + 1, len(pairs)):
         if (int(pairs[i][1]) - int(pairs[j][0])) % 2 == 1:
             new_list.append([pairs[i][1],pairs[j][0]])
             #print(pairs[i][1], pairs[j][0])
@@ -17,5 +17,9 @@ for i in range(len(pairs) - 1):
             new_list.append([pairs[i][1],pairs[j][1]])
             #print(pairs[i][1], pairs[j][1])
             u += 1
-        
-print(new_list)
+
+for i in new_list:
+    a = tuple(i)
+    new_set.add(a)
+print("List ", len(new_list))
+print("Set ", len(new_set))
